68740fc7cd3bf3e61ca708ca2de8a9abd3f196de
Ashutosh Yadav <ashutoshy595@gmail.com>
2024-11-22 20:11:10 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Rutas de prueba
+const productRoutes = require('./routes/productRoutes');
+app.use('/api/products', productRoutes);
+
+
+// Conectar a MongoDB
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Conectado a MongoDB');
+    app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
+  })
+  .catch((err) => console.error('Error conectando a MongoDB:', err));
